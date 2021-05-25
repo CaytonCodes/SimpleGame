ac879467979c452da3bd52d801cc7cc500ad8cd4
Scotty Cayton <kcayton34@gmail.com>
2021-05-25 13:29:48 -0600

    Started updating BoardCont.jsx to hooks

--- client/src/components/BoardCont.jsx
+++ client/src/components/BoardCont.jsx
@@ -1,7 +1,7 @@
 /*
 To be cleaned up
 */
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import GameCanvas from './GameLogic/GameCanvas';
 import StartScreen from './StartScreen';
@@ -29,7 +29,21 @@ const getBoardSize = () => {
   return { boardWidth, boardHeight };
 };
 
-class BoardCont extends React.Component {
+function BoardCont(props) {
+  const [gamePhase, setGamePhase] = useState();
+  const [lastGame, setLastGame] = useState();
+
+  const gameChange = (phase, time = 0) => {
+    const { gameEnd } = props;
+    if (time > 0) {
+      setLastGame(time);
+      gameEnd(time);
+    }
+    setGamePhase(phase);
+  };
+}
+
+class BoardContOld extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -87,3 +101,60 @@ class BoardCont extends React.Component {
 }
 
 export default BoardCont;
+
+// class BoardCont extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       gamePhase: 0,
+//       lastGame: 0,
+//     };
+//     this.gameChange = this.gameChange.bind(this);
+//   }
+
+//   gameChange(phase, time = 0) {
+//     const { gameEnd } = this.props;
+//     if (time > 0) {
+//       this.setState({ lastGame: time });
+//       gameEnd(time);
+//     }
+//     this.setState({ gamePhase: phase });
+//   }
+
+//   gameShow() {
+//     const { gamePhase, lastGame } = this.state;
+//     const { playerName, updatePlayer } = this.props;
+//     if (gamePhase === 0) {
+//       return <StartScreen gameChange={this.gameChange} />;
+//     }
+//     if (gamePhase === 1) {
+//       if (!playerName) {
+//         return (<PlayerModal updatePlayer={updatePlayer} />);
+//       }
+//       const { boardWidth, boardHeight } = getBoardSize();
+//       return (
+//         <GameCanvas
+//           boardWidth={boardWidth}
+//           boardHeight={boardHeight}
+//           gameChange={this.gameChange}
+//         />
+//       );
+//     }
+//     return (
+//       <GameEndScreen
+//         gameTime={lastGame}
+//         gameChange={this.gameChange}
+//       />
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <BoardContainer className="BoardContainer">
+//         <BoardWrapper id="BoardWrapper">
+//           {this.gameShow()}
+//         </BoardWrapper>
+//       </BoardContainer>
+//     );
+//   }
+// }
